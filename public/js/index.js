8550424ce539da1250d4b67d88b94ab664fab737
System Administrator <root@student-10-201-33-216.wireless.adelaide.edu.au>
2022-03-07 14:13:58 +1030

    INQUIRER STILL NOT WORKING PROPERLY

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
 const inquirer = require('inquirer');
 // const inquirerIndex = () => {
         //Prompts to specify which object should be created
-        const askQuestion = () => {
-            inquirer.prompt([
+
+    async function main() {
+
+        
+        const askQuestion = (await inquirer.prompt([
             {
                 type: 'list',
                 name: 'Action',
@@ -10,105 +13,27 @@ const inquirer = require('inquirer');
                 message: 'What would you like to do?',
                 choices: ["View all Departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an Employee", "Update an employee"],
             }
-    
-            // {
-            //     type: 'input',
-            //     name: 'name',
-    
-            //     message: 'Enter the name of the team member!',
-            // },
-    
-            // {
-            //     type: 'input',
-            //     name: 'employeeID',
-            //     message: 'Enter their employee ID',
-            // },
-            // {
-    
-            //     type: 'email',
-            //     name: 'email',
-        
-            //     message: 'Enter their email',
-            // },
-            // {
-        
-            //     type: 'input',
-            //     name: 'officeNumber',
-        
-            //     message: 'Enter their office number ',
-    
-            // },
-            // {
-            //     type: "list",
-            //     name: 'nextSelection',
-    
-            //     message: "What do you want to do next?",
-            //     choices: ["Add another team member", "Thats all!"]
-            // }
-    ])
-        
-            .then((answers) => {
-                handleAnswers(answers);
-            });                                                                                                             
-        };
-        askQuestion() 
-
-    
-    
-    const handleAnswers = ({Action}) => {
-        console.log(Action);
-        if (Action === "View all Departments"){
-            
-        }
-
-
-        // console.log("Creating employee... email:" + email + " and officeNumber: "+officeNumber);
-        // if (position === "Manager") {
-        //     console.log("Creating a manager");
-        //     var tempEmployee = new Manager(name, employeeID, email, officeNumber);
-        // }
-        // else if(position === "Engineer"){
-        //     console.log("Creating a Engineer");
-        //     var tempEmployee = new Engineer(name, employeeID, email, officeNumber);
-        // }
-        // else if(position === "Intern"){
-        //     console.log("Creating a Intern");
-        //     var tempEmployee = new Intern(name, employeeID, email, officeNumber);
-        // }
-        // else{
-        //     console.log("error")
-        // }
-        // //var tempEmployee = new Employee(position, name, employeeID, email, officeNumber);
-        
-        // console.log(`New employee created with name ${tempEmployee.name}`);
-        // employeeArray[index] = tempEmployee;
-        // index++;
-    
-        
-        // console.log("employee array is now");
-        // console.log(employeeArray);
-    
-        // //RUNS FUNCTION AGAIN IF ADD TEAM MEMEBER IS SPECIFIED AT END
-        // if (nextSelection === "Add another team member") {
-        //     nodeMan();
-        // }
-        // else{
-        //     console.log("all done!");
-        //     console.log(`total employees are...`)
-        //     console.log(employeeArray);
-        //     //generate html
-        //     //defining string for all key values
-    
-            
-        //     const HTMLpageContent = generateHTML(employeeArray);
-    
-        //     fs.writeFile('index.html', HTMLpageContent, (err) => {
-        //         err ? console.log(err) : console.log('Successfully created html')
-        //     });
-        //     // console.log(HTMLpageContent);
-        // }
+        ])).askQuestion;
     }
+    main();
+
+        
+    //         .then((answers) => {
+    //             handleAnswers(answers);
+    //         });                                                                                                             
+    //     };
+    //     askQuestion() 
+
+    
+    
+    // const handleAnswers = ({Action}) => {
+    //     console.log(Action);
+    //     if (Action === "View all Departments"){
+
+    //     }
+
+
 // }
 // inquirerIndex();
 
-module.exports = askQuestion;
+module.exports = main;
--- server.js
+++ server.js
@@ -25,76 +25,29 @@ const db = mysql.createConnection(
     database: 'employee_tracker_db'
   },
   console.log(`Connected to the employee_tracker_db database.`)
+  
 );
 
-db.query(`SELECT * FROM employee WHERE id = ?`, 1, (err, result) => {
-  if (err) {
-    console.log(err);
+async function startServer() {
+  try {
+      await app.listen(PORT);
+      console.log(`Server ready at ${PORT}`);
+      const inquirerIndex = require("./public/js/index.js");
+
+  } catch(e) {
+      // handle errors
+      console.log(e);
   }
-  console.log(result);
-});
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-// const routes = require("./routes");
+startServer();
 
 
 
-// const inquirerIndex = require("./public/js/index.js");
-// app.use(inquirerIndex);
-// const routes = require("./routes/index.js");
-// app.use(routes);
-
-
-// app.use(routes);
-
-
-
-
-
-
-
-// const askQuestion = () => {
-//   inquirer.prompt([
-//   {
-//       type: 'list',
-//       name: 'Action',
-
-//       message: 'What would you like to do?',
-//       choices: ["View all Departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an Employee", "Update an employee"],
-//   }])
-        
-//   .then((answers) => {
-//       handleAnswers(answers);
-//   });                                                                                                             
-// };
-// // askQuestion() 
-
-
-
-// const handleAnswers = ({Action}) => {
-// console.log(Action);
-// if (Action === "View all Departments"){
-//   db.query('SELECT * FROM employee', function (err, results) {
-//     console.log(results);
-//   });
-// }
-// }
-
-
-
-// // Query database
-// db.query('SELECT * FROM favorite_books', function (err, results) {
-//   console.log(results);
-// });
-
-// // Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT);
+// db.query(`SELECT * FROM employee`, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.table(result);
 // });
 
