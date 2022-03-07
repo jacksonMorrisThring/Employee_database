349ddd4d9beb7fee946526ed11db38052b94a829
System Administrator <root@Jacksons-MacBook-Air.local>
2022-03-07 14:48:54 +1030

    FIXED IT

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,48 @@
 const inquirer = require('inquirer');
 // const inquirerIndex = () => {
         //Prompts to specify which object should be created
+        const handleAnswers = ({Action}) => {
+            if (Action === "View all Departments"){
+                console.log('Success!');
+            }
+        }
 
-    async function main() {
-
-        
-        const askQuestion = (await inquirer.prompt([
+        const main = (inquirer
+        .prompt([
             {
                 type: 'list',
                 name: 'Action',
-    
+                default: "View all Departments",
                 message: 'What would you like to do?',
                 choices: ["View all Departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an Employee", "Update an employee"],
             }
-        ])).askQuestion;
-    }
-    main();
+        ])
 
+        .then((answers) => {
+            handleAnswers(answers);
+    }));
+
+
+    //     async function main() {
+
+        
+    //         const askQuestion = (await inquirer.prompt([
+    //             {
+    //                 type: 'list',
+    //                 name: 'Action',
+    //                 default: "View all Departments",
+    //                 message: 'What would you like to do?',
+    //                 choices: ["View all Departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an Employee", "Update an employee"],
+    //             }
+
+    //         ]))
         
     //         .then((answers) => {
     //             handleAnswers(answers);
     //         });                                                                                                             
     //     };
-    //     askQuestion() 
-
+    
+    //     main();
     
     
     // const handleAnswers = ({Action}) => {
